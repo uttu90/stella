a795fe9121e7c9a4ebcd9c885c52ce27c2cae461
Hà Văn Tư <hondasuytu.ht@gmail.com>
2017-03-01 22:27:24 +0700

    Update stella

--- stella.py
+++ stella.py
@@ -6,18 +6,18 @@ from pylab import *
 class Stock(object):
     ''''''
     def __init__(self, init_value):
+        self.value = init_value
         self._values = []
-        self._values.append(init_value)
-        self.change_value = init_value
+        self._values.append(self.value)
+        # self.change_value = init_value
 
-    def current_value(self):
-        return self._values[-1]
-
+    @property
     def values(self):
         return self._values
 
     def __call__(self):
-        self._values.append(self.change_value)
+        self._values.append(self.value)
+
 
 class Flow(object):
     ''''''
@@ -26,15 +26,20 @@ class Flow(object):
         self.source_stock = source_stock
         self.destination_stock = destination_stock
         self.time_step = time_step
+        # self._value = None
 
     def __call__(self, *args):
         if self.source_stock is not None:
-            self.source_stock.change_value = self.source_stock.change_value - self.change(args)*self.time_step
+            self.source_stock.value = self.source_stock.value - self.change(args)*self.time_step
 
         if self.destination_stock is not None:
-            self.destination_stock.change_value = self.destination_stock.change_value + self.change(args)*self.time_step
+            self.destination_stock.value = self.destination_stock.value + self.change(args)*self.time_step
 
-        return self.change(args)
+        self._value = self.change(args) * self.time_step
+
+    @property
+    def value(self):
+        return self._value
 
 
 class Converter(object):
@@ -45,32 +50,8 @@ class Converter(object):
         self.convert_function = convert_function
 
     def __call__(self, *args):
-        return self.convert_function(args)
+        self._value = self.convert_function(args)
 
-if __name__ == '__main__':
-
-    def v_flow(x):
-        return x[0]
-
-    def x_flow(x):
-        return x[0]
-
-    init_v = 0
-    init_x = 0
-
-    velocity = Stock(init_v)
-    position = Stock(init_x)
-
-    velocity_flow = Flow(v_flow, None, velocity)
-    position_flow = Flow(x_flow, None, position)
-
-    for i in range(0, 1000):
-        velocity_flow(1)
-        position_flow(velocity.current_value())
-        velocity()
-        position()
-    print(position.values)
-    plot(position.values)
-    show()
-    plot(velocity.values)
-    show()
+    @property
+    def value(self):
+        return self._value
